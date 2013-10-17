5f4d1540e8eeb324c2fcec97889d98961a58b130
Genyaz <genya12321@gmail.com>
2013-10-17 20:54:25 +0400

    The task is done.
    Варламов Евгений Николаевич, 2539.
    Supports RSS, Atom, maybe RDF (don't know sources of rdf).

---
+++ src/com/example/lesson5/RSSResultActivity.java
@@ -0,0 +1,25 @@
+package com.example.lesson5;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Genyaz
+ * Date: 12.10.13
+ * Time: 18:13
+ * To change this template use File | Settings | File Templates.
+ */
+public class RSSResultActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+        WebView webView = (WebView) findViewById(R.id.resultWebView);
+        Intent intent = getIntent();
+        String html = intent.getStringExtra("html");
+        webView.loadData(html, "text/html", null);
+    }
+}
