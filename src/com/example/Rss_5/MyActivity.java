a2fb21f040ebdfa4a82d8ac81cb4b1ba067d0640
bear26 <bear26_rus@mail.ru>
2013-12-29 16:19:46 +0400

    commit 6

--- src/com/example/Rss_5/MyActivity.java
+++ src/com/example/Rss_5/MyActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.*;
 import org.xml.sax.Attributes;
@@ -209,6 +208,7 @@ public class MyActivity extends Activity {
 
     void refresh() {
         Records.clear();
+        rsslistView.setAdapter(arrayAdapter);
         mt = new MyTask();
         mt.execute();
     }
